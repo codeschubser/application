b28b535b9616b8fde73fad9d5994e10995f8224a
Michael Topp <blog@codeschubser.de>
2015-10-22 11:52:10 +0200

    Add SessionHandlerInterface methods

--- libraries/models/Session.php
+++ libraries/models/Session.php
@@ -67,4 +67,35 @@ class Session implements \SessionHandlerInterface
     {
         return session_set_save_handler($this, true);
     }
+
+    public function close()
+    {
+
+    }
+
+    public function destroy($session_id)
+    {
+
+    }
+
+    public function gc($maxlifetime)
+    {
+
+    }
+
+    public function open($save_path, $name)
+    {
+
+    }
+
+    public function read($session_id)
+    {
+
+    }
+
+    public function write($session_id, $session_data)
+    {
+
+    }
+
 }
